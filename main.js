9206ae4cb759a48e5932b13c4bc906fb01cce783
senwere <simeonenwere@gmail.com>
2023-06-21 11:10:47 +0100

    analog clock stage 1

---
+++ main.js
@@ -0,0 +1,11 @@
+let sec=document.getElementById('sec')
+let min=document.getElementById('min')
+let hr=document.getElementById('hr')
+
+console.log(date)
+
+function cock(){
+    let date=new Date()
+    //get the seconds
+    sec=date.getSeconds()
+}
