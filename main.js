7ac28e14c6ddfd578fa812e8a9ade3c422f20b31
senwere <simeonenwere@gmail.com>
2023-06-23 13:24:40 +0100

    ANALOG CLOCK

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ let sec=document.getElementById('sec')
 let min=document.getElementById('min')
 let hr=document.getElementById('hr')
 
-console.log(date)
+setInterval(()=>{
+const date= new Date;
+const secdg=date.getSeconds()/60*360-90;
+sec.style.transform=`rotate(${secdg}deg)`;
 
-function cock(){
-    let date=new Date()
-    //get the seconds
-    sec=date.getSeconds()
-    console.log(sec)
-}
+const mindg=date.getMinutes()/60*360-90;
+
+min.style.transform=`rotate(${mindg}deg)`;
+
+const hrdg=date.getHours()/12*360-90;
+hr.style.transform=`rotate(${hrdg}deg)`;
+})
